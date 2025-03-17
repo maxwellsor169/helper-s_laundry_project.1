14aed35f08c4a76ce91ec98920fc3bf177e8dd06
atm2chx3@students.codeinstitute.net <atm2chx3@students.codeinstitute.net>
2025-03-17 17:15:58 +0000

    Create the home app and the base template

--- helper/settings.py
+++ helper/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #App
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +58,9 @@ ROOT_URLCONF = 'helper.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
